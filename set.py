c14760f8b7aa8e01b10bb850ca569f4b6e70eb0d
Haresh Singh <harishrsingh22@gmail.com>
2020-04-17 17:37:11 +0530

    set ver 1.0

---
+++ set.py
@@ -0,0 +1,11 @@
+# SET data type # Set is setting up onorderd data collection 
+# set is not taking the Duplicate Data 
+#  Set type with { }  Curly Bracket 
+# its provide you random data 
+data = {10, 20, 30, 40, "Haresh", 10,30,20 }
+print(data)
+
+data1 = {10,10,10,20,20,202,20,202, "Haresh", 'Haresh'}
+print(data1) 
+
+
