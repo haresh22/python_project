6c538268beb31c5a1cff1e399695d2131b1f8bdb
Haresh Singh <harishrsingh22@gmail.com>
2020-04-17 22:44:16 +0530

    flotdivision ver 1.0

---
+++ flotdivision.py
@@ -0,0 +1,18 @@
+# Type 1 ( // ) is use for flot or intjer division 
+
+a = 14 
+b = 2 
+value = (a // b)
+print(value)
+
+# Type 2 
+a = 31
+b = -2
+value = (a // b)
+print(value)
+
+#type 3 
+a = 1224
+b = 23
+value = (a // b)
+print(value)
