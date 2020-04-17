5c1f33036e9b359956d11bdc9f77082b5036b873
Haresh Singh <harishrsingh22@gmail.com>
2020-04-17 15:56:50 +0530

    range ver 1.0

---
+++ range.py
@@ -0,0 +1,14 @@
+
+data = range(5)
+
+print(data[0])
+print(data[1])
+print(data[2])
+print(data[3])
+print(data[4])
+
+print(data[-1])
+print(data[-2])
+print(data[-3])
+print(data[-4])
+print(data[-5])
