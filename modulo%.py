ccc2763dc2be1bdba8597bedead034833682210c
Haresh Singh <harishrsingh22@gmail.com>
2020-04-17 22:16:51 +0530

    modulo ver 1.0

---
+++ modulo%.py
@@ -0,0 +1,24 @@
+# Type 1 ( Modulo Means Reminder Value) It gives you full number insted of flot no 
+
+a = 5
+b = 2
+value = (a % b)
+print(value)
+
+# Type 2 
+a = 100
+b = 98
+value = (a % b)
+print(value)
+
+# Type 3
+a = 987
+b = 32
+value = (a % b)
+print(value)
+
+# Type 4 
+a = 20
+b = 7
+value = (a % b)
+print(value)
