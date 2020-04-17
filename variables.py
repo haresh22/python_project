68938bee61a42ef3afb75c353c8844abb9167035
Haresh Singh <harishrsingh22@gmail.com>
2020-04-17 18:56:06 +0530

    variable ver 1.1

--- variables.py
+++ variables.py
@@ -11,5 +11,37 @@ print(_fruit_name)
 # print(12name)
 
 #######################
+# Type 1
+a = 10
+b = 10 
+c = 10 
+d = 10 
+print(a)
+print(b)
+print(c)
+print(d)    
+
+# Type 2
+h = i = j = k = 1000
+print(h)
+print(i)
+print(j)
+print(k)
+
+# Type 3 
+a = b = c = d = True
+print(a)
+print(b)
+print(c)
+print(d)
 
 
+# Type 4 
+
+a, b, c, d, e = 100,  100.20, "Haresh", 100000, "Manisha"
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
