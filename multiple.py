11f7fe2323be1c8cfc8ed3b45e8faeecca043a9f
Haresh Singh <harishrsingh22@gmail.com>
2020-04-17 21:48:32 +0530

    multiple ver 1.0

---
+++ multiple.py
@@ -0,0 +1,25 @@
+# Type 1
+a = 4 
+b = 5 
+total = (a*b)
+print(total)
+
+# Type 2
+
+a = 9 
+b = 8 
+total = (a*b)
+print(total)
+
+# Type 3
+a = 1245
+b = 2568
+total = (a*b)
+print(total)
+
+#Type 4 
+a = 123456
+b = 980976
+c = 132452
+total = (a*b*c)
+print(total)
