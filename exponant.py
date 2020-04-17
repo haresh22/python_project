d01701197c0a0d191facf10f11e7b4597b731bce
Haresh Singh <harishrsingh22@gmail.com>
2020-04-17 22:30:28 +0530

    exponant ver 1.0

---
+++ exponant.py
@@ -0,0 +1,31 @@
+# Type 1 Its gives the POWER eg power of 2 , power of 3 it means multiply with same figure
+
+a = 5 
+b = 2 
+value = (a ** b )
+print(value)
+
+# type 2 
+a = 21 
+b = 2 
+value = (a ** b)
+print(value)
+
+# Type 3 
+a = 5 
+b = 4 
+value = (a ** b)
+print(value)
+
+# Type 4 
+a = 123
+b = 5 
+value = (a ** b)
+print(value)
+
+# Type 5 
+a = 5
+b = 2
+c = 2
+value = (a ** b ** c)
+print(value)
