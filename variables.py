63e11545bb8273556c7175cfe6113d126e37b486
Haresh Singh <harishrsingh22@gmail.com>
2020-04-17 12:08:00 +0530

    new file variable

---
+++ variables.py
@@ -0,0 +1,15 @@
+### Right Sysntax ####
+name = "White Devil "
+name_city = "Mumbai"
+_fruit_name = "Mango" 
+print (name)
+print(name_city)
+print(_fruit_name)
+#### Wrong Syntax ####
+
+# 12name = "White Devil "
+# print(12name)
+
+######################
+
+
