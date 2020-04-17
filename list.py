96671510e4f793b4d868d0db0f5acedda6b18aac
Haresh Singh <harishrsingh22@gmail.com>
2020-04-17 13:15:45 +0530

    list commit

---
+++ list.py
@@ -0,0 +1,12 @@
+# List #
+
+data = [10,20,30,10.51,'Life is Awesomoe']
+print(data[-1])
+print(data[-2])
+print(data[-3])
+print(data[-4])
+print(data[-5])
+print(data[1])
+print(data[2])
+print(data[3])
+print(data[4])
