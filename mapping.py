f53f2d63898a7e92fa7ab454ff2f73c03649002e
Haresh Singh <harishrsingh22@gmail.com>
2020-04-17 18:01:22 +0530

    mapping dict ver 1.0

---
+++ mapping.py
@@ -0,0 +1,14 @@
+
+# Mapping tupe Data called Map / Dict / Disctonery 
+# Curly Bracket {}
+
+
+data = {100: "Haresh", 200: "Manisha", 300: "Dipesh"}
+print(data)
+print(data[100])
+print(data[200])
+print(data[300])
+
+
+mk = {'Haresh' :100, "Manisha":200,}
+print(mk["Manisha"])
